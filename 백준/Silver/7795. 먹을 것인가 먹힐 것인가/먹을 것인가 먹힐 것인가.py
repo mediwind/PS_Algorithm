ee0f93c93d1c612b421e902c784e68dc458e4c58
mediwind <48349059+mediwind@users.noreply.github.com>
2024-03-23 23:47:46 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 144 ms, Memory: 34640 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,28 +1,23 @@
-def bs(arr, x):
-    lt, rt = 0, len(arr) - 1
-    res = float('-inf')
-    while lt <= rt:
-        m = (lt + rt) // 2
-        if arr[m] < x:
-            res = max(res, m)
-            lt = m + 1
-        else:
-            rt = m - 1
-    
-    if res == float('-inf'):
-        return 0
-    return res + 1
+T = int(input())
 
+for i in range(T):
+    N,M = map(int,input().split())
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
 
-t = int(input())
-for _ in range(t):
-    n, m = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    a.sort()
-    b.sort()
+    A.sort()
+    B.sort()
 
-    answer = 0
-    for i in a:
-        answer += bs(b, i)
-    print(answer)
+    start = 0
+    count = 0
+ 
+    for j in range(N):
+        while True:
+
+            if start==M or A[j]<=B[start]:
+                count+=start
+                break
+            else:   
+                start+=1
+                
+    print(count)
