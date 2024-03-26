02955d1768cd2d8070510578cb9e8bd70e5c6d43
mediwind <48349059+mediwind@users.noreply.github.com>
2024-03-26 21:40:46 +0900

    Time: 43 ms (17.7%), Space: 16.5 MB (93.17%) - LeetHub

---
+++ LeetCode/Easy/0027-remove-element/0027-remove-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        tmp = list()
+        k = 0
+        for v in nums:
+            if v != val:
+                tmp.append(v)
+                k += 1
+        tmp.sort()
+        nums[:] = tmp[:]
+        # print('nums:', nums)
+        # print('k', k)
+        return k
