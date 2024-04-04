6f0385355e69e9044ad74ba0241fa21d989f60b1
mediwind <48349059+mediwind@users.noreply.github.com>
2024-04-04 20:09:20 +0900

    Update 카드게임.py

--- 백준/Gold/10835. 카드게임/카드게임.py
+++ 백준/Gold/10835. 카드게임/카드게임.py
@@ -2,6 +2,7 @@ n = int(input())
 left = list(map(int, input().split()))
 right = list(map(int, input().split()))
 
+# dy[i][j]는 왼쪽 카드 더미 0번 인덱스, 오른쪽 카드 더미 j번 인덱스부터 게임을 진행할 때 얻을 수 있는 최대 점수
 dy = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
 for i in range(n-1, -1, -1):
