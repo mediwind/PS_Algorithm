cf4b71806dbeaef2fb486fbf932671c0dc62b6e7
mediwind <48349059+mediwind@users.noreply.github.com>
2024-04-10 10:53:20 +0900

    [Gold IV] Title: 괄호, Time: 2060 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.py
@@ -0,0 +1,14 @@
+# 10422 
+dy = [0 for _ in range(5001)]
+dy[0] = 1
+mod = 1000000007
+
+for i in range(2, 5001, 2):
+    for j in range(2, 5001, 2):
+        dy[i] += dy[j - 2] * dy[i - j]
+        dy[i] %= mod
+
+t = int(input())
+for _ in range(t):
+    l = int(input())
+    print(dy[l])
