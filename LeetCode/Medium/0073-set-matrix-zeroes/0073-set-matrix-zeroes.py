a2b27d01f9c00f4750646799d9216eeecb277903
mediwind <48349059+mediwind@users.noreply.github.com>
2024-04-29 12:19:00 +0900

    Time: 100 ms (72.13%), Space: 17.6 MB (28.58%) - LeetHub

---
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = set()
+        col = set()
+
+        n, m = len(matrix), len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    row.add(i)
+                    col.add(j)
+        
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] and (i in row or j in col):
+                    matrix[i][j] = 0
+        
+        return matrix
