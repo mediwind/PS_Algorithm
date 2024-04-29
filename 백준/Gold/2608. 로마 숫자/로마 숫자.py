0199bd298c2082cbb5b9563e5e67631370b85286
lyh <mediwind@gmail.com>
2024-04-29 14:30:28 +0900

    Update 로마 숫자.py

--- 백준/Gold/2608. 로마 숫자/로마 숫자.py
+++ 백준/Gold/2608. 로마 숫자/로마 숫자.py
@@ -1,4 +1,31 @@
-roman = {
+def romanToInt(s):
+    ans = 0
+
+    for i in range(len(s)):
+        if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+            ans -= m[s[i]]
+        else:
+            ans += m[s[i]]
+
+    return ans
+
+
+def intToRoman(num):
+    answer = ''
+    for key in list(roman.keys())[::-1]:
+        if key > num:
+            continue
+
+        cnt = num // key
+        # print(num, key, cnt)
+        answer += roman[key] * cnt
+        num %= key
+        # print(num)
+
+    return answer
+
+
+m = {
     'I': 1,
     'V': 5,
     'X': 10,
@@ -8,47 +35,25 @@ roman = {
     'M': 1000
 }
 
-number = {
-    1: 'I',
-    4: 'IV',
-    5: 'V',
-    9: 'IX',
-    10: 'X',
-    40: 'XL',
-    50: 'L',
-    90: 'XC',
-    100: 'C',
-    400: 'CD',
-    500: 'D',
-    900: 'CM',
+roman = {
+    1: 'I', 
+    4: 'IV', 
+    5: 'V', 
+    9: 'IX', 
+    10: 'X', 
+    40: 'XL', 
+    50: 'L', 
+    90: 'XC', 
+    100: 'C', 
+    400: 'CD', 
+    500: 'D', 
+    900: 'CM', 
     1000: 'M'
 }
 
-ans = 0
+number = 0
 for _ in range(2):
     string = input()
-    stack = list()
-    for s in string:
-        if stack and roman[stack[-1]] < roman[s]:
-            ans += roman[s] - roman[stack[-1]]*2
-        else:
-            ans += roman[s]
-        stack.append(s)
-print(ans)
-
-num = ans
-n = len(str(num))
-alpha = ''
-for i in range(1, n + 1):
-    place = 10 ** (n - i)
-    value = num // place
-#     print(i, place, value)
-    if place*value in number:
-        alpha += number[place*value]
-    else:
-        if value < 5:
-            alpha += number[place] * value
-        elif value > 5:
-            alpha += number[5*place] + (value-5)*number[place]
-    num -= value * place
-print(alpha)
+    number += romanToInt(string)
+print(number)
+print(intToRoman(number))
