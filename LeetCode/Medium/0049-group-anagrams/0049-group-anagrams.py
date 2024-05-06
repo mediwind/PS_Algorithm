794e991a5eff2f359beb48f932b6caeeac48f833
lyh <mediwind@gmail.com>
2024-05-06 12:55:11 +0900

    Update 0049-group-anagrams.py

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,11 @@
 from collections import defaultdict
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_strs = list(map(sorted, strs))
-        sorted_strs = list(map(''.join, sorted_strs))
-        sorted_strs
-
-        words = defaultdict(list)
-        for idx, val in enumerate(sorted_strs):
-            words[val].append(idx)
-
-        ans = list()
-        for val in words.values():
-            tmp = list()
-            for v in val:
-                tmp.append(strs[v])
-            ans.append(tmp)
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
         
-        return ans
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())
