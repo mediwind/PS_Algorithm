8ae25610938e5061046079e45a3537911eea94b1
mediwind <48349059+mediwind@users.noreply.github.com>
2024-05-10 15:03:30 +0900

    Time: 29 ms (87.78%), Space: 16.5 MB (89.35%) - LeetHub

--- LeetCode/Easy/0228-summary-ranges/0228-summary-ranges.py
+++ LeetCode/Easy/0228-summary-ranges/0228-summary-ranges.py
@@ -5,20 +5,25 @@ class Solution:
             return list()
         
         ans = list()
-        start, end = nums[0], nums[0]
+        start = nums[0]
+        cnt = 0
         for i in range(1, n):
-            if nums[i] == end + 1:
-                end = nums[i]
+            if nums[i] == nums[i - 1] + 1:
+                cnt += 1
             else:
-                if start == end:
-                    ans.append(str(start))
+                if cnt > 0:
+                    ans.append(f'{start}->{start + cnt}')
                 else:
-                    ans.append(f'{start}->{end}')
-                start, end = nums[i], nums[i]
+                    ans.append(str(start))
+                start = nums[i]
+                cnt = 0
 
-        if start == end:
+        print(ans)
+        print(start, cnt)
+
+        if start == start + cnt: # that is cnt is 0
             ans.append(str(start))
         else:
-            ans.append(f'{start}->{end}')
+            ans.append(f'{start}->{start + cnt}')
 
         return ans
