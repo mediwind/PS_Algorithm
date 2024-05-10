cee190a89bf5c37a6c47ff0d4b5ffe37655b3967
mediwind <48349059+mediwind@users.noreply.github.com>
2024-05-10 16:41:13 +0900

    [Gold II] Title: 환승, Time: 1944 ms, Memory: 134696 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.py
+++ 백준/Gold/5214. 환승/환승.py
@@ -1,56 +1,36 @@
 from collections import defaultdict
 
-import heapq as hq
-import sys
-input = sys.stdin.readline
+import heapq
+# input = sys.stdin.readline
 
+N, K, M = map(int, input().split())
+INF = float('inf')
 
-class CompareFirst:
-    def __init__(self, tup):
-        self.tup = tup
-
-    def __lt__(self, other):
-        return self.tup[0] < other.tup[0]
-
-    def __eq__(self, other):
-        return self.tup[0] == other.tup[0]
-
-    def __repr__(self):
-        return repr(self.tup)
-
-
-def djikstra(x):
-    Q = list()
-    hq.heappush(Q, CompareFirst((0, x)))
-    dy[x] = 0
-    
-    while Q:
-        cost, node = hq.heappop(Q).tup
-        if dy[node] < cost:
-            continue
-        
-        for next_node, next_cost in graph[node]:
-            next_cost += cost
-            if dy[next_node] > next_cost:
-                dy[next_node] = next_cost
-                hq.heappush(Q, CompareFirst((next_cost, next_node)))                
-
-
-n, k, m = map(int, input().split())
 graph = defaultdict(list)
-for i in range(1, m + 1):
-    stations = list(map(int, input().split()))
-    for st in stations:
-        graph[st].append((str(i), 0))
-        graph[str(i)].append((st, 1))
+cost = [INF] * (N + M)
 
-dy = dict()
-for i in range(1, n + 1):
-    dy[i] = float('inf')
-for i in range(1, m + 1):
-    dy[f'{i}'] = float('inf')
-djikstra(1)
-if dy[n] != float('inf'):
-    print(dy[n] + 1)
+for m in range(M):
+    stations = list(map(int, input().split()))
+    for n in stations:
+        n -= 1
+        graph[n].append(N + m)
+        graph[N + m].append(n)
+
+heap = [(0, 0)]
+cost[0] = 0
+
+while heap:
+    cur_cost, cur = heapq.heappop(heap)
+    if cost[cur] < cur_cost:
+        continue
+
+    for next_node in graph[cur]:
+        distance = 0 if next_node >= N else 1
+        if cost[next_node] > cost[cur] + distance:
+            cost[next_node] = cost[cur] + distance
+            heapq.heappush(heap, (cost[next_node], next_node))
+
+if cost[N - 1] != INF:
+    print(cost[N - 1] + 1)
 else:
     print(-1)
