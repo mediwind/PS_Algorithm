a496173c3e1be37788a19b676fb930225417f3ca
mediwind <48349059+mediwind@users.noreply.github.com>
2024-05-16 13:19:03 +0900

    Time: 65 ms (16.12%), Space: 20.1 MB (93.13%) - LeetHub

---
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.py
@@ -0,0 +1,36 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+        self.min_stack = list()
+    
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            self.min_stack.append(min(val, self.min_stack[-1]))
+                
+    # Methods pop, top and getMin operations will always be called on non-empty stacks.
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
