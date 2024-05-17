e49e532138e54676e37ead226a30c7668514f583
mediwind <48349059+mediwind@users.noreply.github.com>
2024-05-17 13:02:47 +0900

    Time: 111 ms (5.45%), Space: 16.9 MB (99.76%) - LeetHub

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,27 +1,14 @@
 from math import ceil, floor
 
-
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = list()
         for t in tokens:
-            if is_number(t):
+            if t not in ['+', '-', '*', '/']:
                 stack.append(t)
             else:
                 b, a = stack.pop(), stack.pop()
-                res = eval(''.join([a, t, b]))
-                if res >= 0:
-                    res = floor(res)
-                else:
-                    res = ceil(res)
+                res = int(eval(''.join([a, t, b])))
                 stack.append(str(res))
         
         return int(stack[0])
