55dada20469498c72300485f7b6a41214a996866
mediwind <48349059+mediwind@users.noreply.github.com>
2024-05-17 13:08:18 +0900

    Time: 112 ms (5.19%), Space: 17.1 MB (33.23%) - LeetHub

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,5 +1,3 @@
-from math import ceil, floor
-
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = list()
@@ -8,6 +6,8 @@ class Solution:
                 stack.append(t)
             else:
                 b, a = stack.pop(), stack.pop()
+                # The division between two integers always truncates toward zero.
+                # 소수점이 0의 방향으로 수렴되게 하려면 floor()가 아닌 int()
                 res = int(eval(''.join([a, t, b])))
                 stack.append(str(res))
         
