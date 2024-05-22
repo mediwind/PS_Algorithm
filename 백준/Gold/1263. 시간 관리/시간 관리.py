ed5602508286fc7fbc9a64bd42a13539e7f49d1c
lyh <mediwind@gmail.com>
2024-05-22 14:05:20 +0900

    Update 시간 관리.py

--- 백준/Gold/1263. 시간 관리/시간 관리.py
+++ 백준/Gold/1263. 시간 관리/시간 관리.py
@@ -1,35 +1,22 @@
-import sys
-input = sys.stdin.readline
-
-
 def simulation(hour):
-    start = hour
-    end = start + works[0][0]
-    for i in range(1, N):
-        if works[i][1] - works[i][0] < end:
+    for work in works:
+        hour += work[0]
+        if hour > work[1]:
             return False
-        else:
-            start = end
-            end = start + works[i][0]
-    
     return True
-    
 
 N = int(input())
 works = [list(map(int, input().split())) for _ in range(N)]
-works.sort(key = lambda x: (x[1], x[0]))
+works.sort(key=lambda x: (x[1], x[0]))
 
-res = float('-inf')
+ans = -1
 lt, rt = 0, works[0][1] - works[0][0]
 while lt <= rt:
     mid = (lt + rt) // 2
     if simulation(mid):
         lt = mid + 1
-        res = max(res, mid)
+        ans = mid
     else:
         rt = mid - 1
 
-if res == float('-inf'):
-    print(-1)
-else:
-    print(res)
+print(ans)
