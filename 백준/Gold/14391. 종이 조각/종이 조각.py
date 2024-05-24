a6c79e0e2af8f2b44cd8a1063c4d9b96074ef906
mediwind <48349059+mediwind@users.noreply.github.com>
2024-05-24 14:15:59 +0900

    [Gold III] Title: 종이 조각, Time: 352 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.py
+++ 백준/Gold/14391. 종이 조각/종이 조각.py
@@ -1,33 +1,49 @@
 n, m = map(int, input().split())
+arr = [list(map(int, list(input()))) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
 
-paper = [list(map(int, input())) for _ in range(n)]
-ans = list()
-
-for i in range(1 << n*m):
+def total_sum():
     total = 0
-    # [Case 1] Calculate horizontal combinations
-    for x in range(n):
-        rowsum = 0
-        for y in range(m):
-            idx = x*m + y
-            if i & (1 << idx) != 0:
-                rowsum = rowsum * 10 + paper[x][y]
-            else:
-                total += rowsum
-                rowsum = 0
-        total += rowsum
 
-    # [Case 2] Calculate vertical combinations
-    for y in range(m):
-        colsum = 0
-        for x in range(n):
-            idx = x*m + y
-            if i & (1 << idx) == 0:
-                colsum = colsum * 10 + paper[x][y]
+    for row in range(n):
+        _sum = 0
+        for col in range(m):
+            if visited[row][col]:
+                _sum = _sum * 10 + arr[row][col]
             else:
-                total += colsum
-                colsum = 0
-        total += colsum
-    ans.append(total)
+                total += _sum
+                _sum = 0
+        total += _sum
 
-print(max(ans))
+    for col in range(m):
+        _sum = 0
+        for row in range(n):
+            if not visited[row][col]:
+                _sum = _sum * 10 + arr[row][col]
+            else:
+                total += _sum
+                _sum = 0
+        total += _sum
+
+    return total
+
+result = 0
+
+def dfs(x, y):
+    global result
+    if x == n:
+        result = max(result, total_sum())
+        return
+
+    if y == m:
+        dfs(x + 1, 0)
+        return
+
+    visited[x][y] = True
+    dfs(x, y + 1)
+
+    visited[x][y] = False
+    dfs(x, y + 1)
+
+dfs(0, 0)
+print(result)
