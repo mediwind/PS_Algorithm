b3c7d06e096af643a34cfacb817628f8304b732a
lyh <mediwind@gmail.com>
2024-06-18 12:27:21 +0900

    Update Swapity Swap.py

--- 백준/Silver/18788. Swapity Swap/Swapity Swap.py
+++ 백준/Silver/18788. Swapity Swap/Swapity Swap.py
@@ -13,13 +13,13 @@ B1, B2 = map(int, input().split())
 answer = [0 for _ in range(N + 1)]
 
 for i in range(1, N + 1):
-    cur = moving(i)
-    needed_turn = 1
-    while cur != i:
+    cur = moving(i) # 일단 1턴 움직입니다.
+    needed_turn = 1 # 그래서 1
+    while cur != i: # 다시 제자리로 돌아올 때까지
         cur = moving(cur)
-        needed_turn += 1
-    
+        needed_turn += 1 # 턴을 늘리며 움직입니다.
     k = K % needed_turn
+    
     cur = i
     for _ in range(k):
         cur = moving(cur)
