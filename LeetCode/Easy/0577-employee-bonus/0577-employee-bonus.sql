b48f46e8589ff55f1f422491f1d6286659aedf8f
mediwind <48349059+mediwind@users.noreply.github.com>
2024-08-31 10:02:39 +0900

    Time: 754 ms (97.97%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0577-employee-bonus/0577-employee-bonus.sql
+++ LeetCode/Easy/0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
-SELECT e.name, bonus
-FROM employee AS e
-LEFT OUTER JOIN bonus AS b
-ON e.empId = b.empId
-WHERE COALESCE(b.bonus, 0) < 1000
+-- SELECT e.name, bonus
+-- FROM employee AS e
+-- LEFT OUTER JOIN bonus AS b
+-- ON e.empId = b.empId
+-- WHERE COALESCE(b.bonus, 0) < 1000
+
+SELECT Employee.name,Bonus.bonus FROM Employee 
+LEFT JOIN Bonus ON Employee.empID = Bonus.empID
+WHERE bonus < 1000 OR Bonus IS NULL ;
