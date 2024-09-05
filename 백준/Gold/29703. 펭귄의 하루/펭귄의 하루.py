211d10ffbf91730919617c9f68aabe870973a65e
mediwind <48349059+mediwind@users.noreply.github.com>
2024-09-05 10:57:09 +0900

    [Gold IV] Title: 펭귄의 하루, Time: 2484 ms, Memory: 126984 KB -BaekjoonHub

---
+++ 백준/Gold/29703. 펭귄의 하루/펭귄의 하루.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+def BFS(n, m, board):
+    ch = [[[False, False] for _ in range(m)] for _ in range(n)]
+    Q = deque()
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 'S':
+                Q.append(((i, j), 0, 0))
+                ch[i][j][0] = True
+
+    while Q:
+        (y, x), now_t, now_f = Q.popleft()
+
+        if now_f == 1 and board[y][x] == 'H':
+            return now_t
+
+        for d in range(4):
+            yy = y + dy[d]
+            xx = x + dx[d]
+
+            if 0 <= yy < n and 0 <= xx < m and not ch[yy][xx][now_f] and board[yy][xx] != 'D':
+                if now_f == 0:
+                    if board[yy][xx] == 'F':
+                        Q.append(((yy, xx), now_t + 1, 1))
+                        ch[yy][xx][1] = True
+                    else:
+                        Q.append(((yy, xx), now_t + 1, 0))
+                        ch[yy][xx][0] = True
+                else:
+                    Q.append(((yy, xx), now_t + 1, 1))
+                    ch[yy][xx][1] = True
+
+    return -1
+
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+n, m = map(int, input().split())
+board = [list(input()) for _ in range(n)]
+print(BFS(n, m, board))
