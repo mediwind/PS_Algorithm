e9c0be59fa5496e7db79c54d499680c576076ee8
mediwind <48349059+mediwind@users.noreply.github.com>
2024-09-06 10:59:27 +0900

    [Gold V] Title: Black and White, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11467. Black and White/Black and White.py
+++ 백준/Gold/11467. Black and White/Black and White.py
@@ -1,18 +1,15 @@
 def repeat(s, n):
     return s * n
 
-def run():
-    b, w = map(int, input().split())
-    z = max(b, w)
-    
-    r = 4
-    c = z * 2
-    
-    print(r, c)
-    print(repeat(".@", w - 1) + repeat("@@", z - w + 1))
-    print(repeat("@@", z))
-    print(repeat("..", z))
-    print(repeat(".@", b - 1) + repeat("..", z - b + 1))
 
-if __name__ == "__main__":
-    run()
+b, w = map(int, input().split())
+z = max(b, w)
+
+r = 4
+c = z * 2
+
+print(r, c)
+print(repeat(".@", w - 1) + repeat("@@", z - w + 1))
+print(repeat("@@", z))
+print(repeat("..", z))
+print(repeat(".@", b - 1) + repeat("..", z - b + 1))
