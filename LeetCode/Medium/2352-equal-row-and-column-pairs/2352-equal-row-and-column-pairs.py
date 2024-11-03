2f5392c55e03ba895386b70fa21771e59c3728a6
mediwind <48349059+mediwind@users.noreply.github.com>
2024-11-03 13:41:23 +0900

    Time: 3 ms (99.68%), Space: 21.2 MB (93.45%) - LeetHub

--- LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ LeetCode/Medium/2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,13 +1,11 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        compared = list(map(list, zip(*grid)))
-        grid = sorted(grid)
-        compared = sorted(compared)
-        
-        cnt = 0
-        for g in grid:
-            for c in compared:
-                if g == c:
-                    cnt += 1
-        
-        return cnt
+        # https://leetcode.com/problems/equal-row-and-column-pairs/?envType=study-plan-v2&envId=leetcode-75
+
+        tpse = Counter(zip(*grid))                  # <-- determine the transpose
+                                                    #     and hash the rows
+
+        grid = Counter(map(tuple, grid))             # <-- hash the rows of grid. (Note the tuple-map, so
+                                                    #     we can compare apples w/ apples in next step.)
+
+        return  sum(tpse[t] * grid[t] for t in tpse)  # <-- compute the number of identical pairs
