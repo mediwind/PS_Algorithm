ae896cccc5ae7d261a37ee2eae460808a1051c00
mediwind <48349059+mediwind@users.noreply.github.com>
2024-11-19 16:43:57 +0900

    [Gold IV] Title: Bessie Goes Moo, Time: 656 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/10761. Bessie Goes Moo/Bessie Goes Moo.py
@@ -0,0 +1,21 @@
+n = int(input())
+num = [[0] * 7 for _ in range(256)]
+
+for _ in range(n):
+    letter, val = input().split()
+    val = int(val)
+    num[ord(letter)][(val % 7 + 7) % 7] += 1
+
+result = 0
+
+for B in range(7):
+    for E in range(7):
+        for S in range(7):
+            for I in range(7):
+                for G in range(7):
+                    for O in range(7):
+                        for M in range(7):
+                            if ((B + E + S + S + I + E) * (G + O + E + S) * (M + O + O)) % 7 == 0:
+                                result += num[ord('B')][B] * num[ord('E')][E] * num[ord('S')][S] * num[ord('I')][I] * num[ord('G')][G] * num[ord('O')][O] * num[ord('M')][M]
+
+print(result)
