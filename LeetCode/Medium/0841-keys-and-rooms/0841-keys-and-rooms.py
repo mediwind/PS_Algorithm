e647207ad9ca7f7c90d6ac5a7f60536a0fdb4eee
mediwind <48349059+mediwind@users.noreply.github.com>
2024-11-24 19:08:24 +0900

    Time: 4 ms (16.89%), Space: 17.2 MB (12.11%) - LeetHub

--- LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.py
+++ LeetCode/Medium/0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,23 +1,11 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        def DFS(num):
-            ch[num] = 1
-            for k in rooms[num]:
-                key[k] = 1
-            
-            for i in range(n):
-                if key[i] and not ch[i]:
-                    DFS(i)
-            
-
-        n = len(rooms)
-        key = [0 for _ in range(n)]
-        key[0] = 1
-        ch = [0 for _ in range(n)]
+        def DFS(room):
+            if room not in ch:
+                ch.add(room)
+                for key in rooms[room]:
+                    DFS(key)
+        
+        ch = set()
         DFS(0)
-
-        for i in range(n):
-            if not ch[i]:
-                return False
-                
-        return True
+        return len(ch) == len(rooms)
