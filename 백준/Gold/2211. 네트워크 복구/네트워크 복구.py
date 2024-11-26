1728557a7125a337684ae20af7b551700544d6d0
mediwind <48349059+mediwind@users.noreply.github.com>
2024-11-26 09:23:27 +0900

    [Gold II] Title: 네트워크 복구, Time: 416 ms, Memory: 76976 KB -BaekjoonHub

--- 백준/Gold/2211. 네트워크 복구/네트워크 복구.py
+++ 백준/Gold/2211. 네트워크 복구/네트워크 복구.py
@@ -7,7 +7,7 @@ def dijkstra(x):
     Q = list()
     dist[x] = 0
     hq.heappush(Q, (0, x))
-    prev = [-1] * (n + 1)  # 이전 노드를 기록하는 리스트
+    prev = [-1 for _ in range(n + 1)]
     
     while Q:
         cost, node = hq.heappop(Q)
@@ -15,21 +15,13 @@ def dijkstra(x):
             continue
         
         for next_node, next_cost in graph[node]:
-            new_cost = cost + next_cost
-            if new_cost < dist[next_node]:
-                dist[next_node] = new_cost
-                prev[next_node] = node  # 이전 노드를 기록
-                hq.heappush(Q, (new_cost, next_node))
+            next_cost += cost
+            if next_cost < dist[next_node]:
+                dist[next_node] = next_cost
+                prev[next_node] = node
+                hq.heappush(Q, (next_cost, next_node))
     
     return prev
-
-
-def find_paths(prev):
-    paths = list()
-    for node in range(2, n + 1):
-        if prev[node] != -1:
-            paths.append((prev[node], node))
-    return paths
 
 
 n, m = map(int, input().split())
@@ -41,7 +33,11 @@ for _ in range(m):
 
 dist = [float('inf') for _ in range(n + 1)]
 prev = dijkstra(1)
-paths = find_paths(prev)
+paths = list()
+for node in range(2, n + 1):
+    if prev[node] != -1:
+        paths.append((prev[node], node))
+
 print(len(paths))
 for a, b in paths:
     print(a, b)
