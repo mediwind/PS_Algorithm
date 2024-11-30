be2b0351691930b47e9fdbfaaa5886ea0ff6ce65
mediwind <48349059+mediwind@users.noreply.github.com>
2024-11-30 09:49:19 +0900

    [Gold V] Title: 가희와 은행, Time: 652 ms, Memory: 88556 KB -BaekjoonHub

--- 백준/Gold/22234. 가희와 은행/가희와 은행.py
+++ 백준/Gold/22234. 가희와 은행/가희와 은행.py
@@ -4,38 +4,26 @@ input = sys.stdin.readline
 
 n, t, w = map(int, input().split())
 Q = deque(tuple(map(int, input().split())) for _ in range(n))
-Q
 
 m = int(input())
 latter = dict()
 for _ in range(m):
     px, tx, cx = map(int, input().split())
     latter[cx] = (px, tx)
-latter
 
 second = 0
 while second < w:
     order = Q.popleft()
     _id, time = order[0], order[1]
     
+    tmp = list()
+    for _ in range(min(time, t)):
+        print(_id)
+        second += 1
+        if second >= w:
+            sys.exit(0)
+        if second in latter.keys():
+            tmp.append(latter[second])
+    Q += tmp
     if time > t:
-        tmp = list()
-        for _ in range(t):
-            print(_id)
-            second += 1
-            if second >= w:
-                sys.exit(0)
-            if second in latter.keys():
-                tmp.append(latter[second])
-        Q += tmp
         Q.append((_id, time - t))
-    else:
-        tmp = list()
-        for _ in range(time):
-            second += 1
-            print(_id)
-            if second >= w:
-                sys.exit(0)
-            if second in latter.keys():
-                tmp.append(latter[second])
-        Q += tmp
