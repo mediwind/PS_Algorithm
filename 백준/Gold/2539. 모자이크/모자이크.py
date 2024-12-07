d99ff9dd3fbf29f793aee0d511f743cd48fca637
mediwind <48349059+mediwind@users.noreply.github.com>
2024-12-07 16:31:30 +0900

    [Gold IV] Title: 모자이크, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2539. 모자이크/모자이크.py
+++ 백준/Gold/2539. 모자이크/모자이크.py
@@ -2,41 +2,33 @@ import sys
 input = sys.stdin.readline
 
 
-def cal_paper_num(col_list, sz):
-    result = 1
-    prev = col_list[0] - 1
-    if len(col_list) > 1:
-        for c in col_list:
-            if prev + sz < c:
-                result += 1
-                prev = c - 1
+def covering(x):
+    cnt = 1
+    prev = columns[0] - 1
+    if len(columns) > 1:
+        for col in columns:
+            if col > prev + x:
+                cnt += 1
+                prev = col - 1
+    
+    return cnt
 
-    return result
 
+r, c = map(int, input().split())
+n = int(input())
+s = int(input())
+spaces = [list(map(int, input().split())) for _ in range(s)]
+columns = list(col for row, col in sorted(spaces, key = lambda x: x[1]))
 
-R, C = map(int, input().split())
-needed_paper = int(input())
-N = int(input())
-
-max_row = 0
-col_list = list()
-for _ in range(N):
-    r, c = map(int, input().split())
-    max_row = max(r, max_row)
-    col_list.append(c)
-
-col_list.sort()
-
-start, end = max_row, R
-
-result = end
-while start <= end:
-    mid = (start + end) // 2
-    used_paper = cal_paper_num(col_list, mid)
-    if used_paper <= needed_paper:
-        result = min(result, mid)
-        end = mid - 1
+lt, rt = max(spaces, key = lambda x: x[0])[0], r
+ans = r
+while lt <= rt:
+    mid = (lt + rt) // 2
+    res = covering(mid)
+    if res <= n:
+        ans = min(ans, mid)
+        rt = mid - 1
     else:
-        start = mid + 1
+        lt = mid + 1
 
-print(result)
+print(ans)
