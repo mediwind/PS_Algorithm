cf7e9aa53a04c3c6f0f8d338718b4c80ede881df
lyh <mediwind@gmail.com>
2024-12-07 16:41:14 +0900

    Update 모자이크.py

--- 백준/Gold/2539. 모자이크/모자이크.py
+++ 백준/Gold/2539. 모자이크/모자이크.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 
 
 def covering(x):
+    ch = {columns[0] + i for i in range(x)}
     cnt = 1
-    prev = columns[0] - 1
-    if len(columns) > 1:
-        for col in columns:
-            if col > prev + x:
-                cnt += 1
-                prev = col - 1
+    for i in range(1, len(columns)):
+        if columns[i] not in ch:
+            for j in range(x):
+                ch.add(columns[i] + j)
+            cnt += 1
     
     return cnt
 
@@ -21,7 +21,7 @@ spaces = [list(map(int, input().split())) for _ in range(s)]
 columns = list(col for row, col in sorted(spaces, key = lambda x: x[1]))
 
 lt, rt = max(spaces, key = lambda x: x[0])[0], r
-ans = r
+ans = float("inf")
 while lt <= rt:
     mid = (lt + rt) // 2
     res = covering(mid)
