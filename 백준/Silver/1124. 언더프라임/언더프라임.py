af4ec5bccecdb23e03872ce7dafb0ff15c7cee5a
mediwind <48349059+mediwind@users.noreply.github.com>
2024-12-22 23:58:29 +0900

    [Silver I] Title: 언더프라임, Time: 228 ms, Memory: 36344 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.py
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -1,12 +1,14 @@
 from math import sqrt
 
 
-def counting(x):
+def counting(x, primes):
     cnt = 0
-    for i in range(2, int(sqrt(x)) + 1):
-        while x % i == 0:
+    for prime in primes:
+        if prime * prime > x:
+            break
+        while x % prime == 0:
             cnt += 1
-            x //= i
+            x //= prime
     
     if x != 1:
         cnt += 1
@@ -17,18 +19,18 @@ def counting(x):
 a, b = map(int, input().split())
 
 nums = [True for _ in range(b + 1)]
-nums[1] = False
+nums[0] = nums[1] = False
 
-for i in range(2, b + 1):
-    if not nums[i]:
-        continue
-        
-    for j in range(i ** 2, b + 1, i):
-        nums[j] = False
+for i in range(2, int(sqrt(b)) + 1):
+    if nums[i]:
+        for j in range(i * i, b + 1, i):
+            nums[j] = False
+
+primes = [i for i in range(2, b + 1) if nums[i]]
 
 ans = 0
 for i in range(a, b + 1):
-    if nums[counting(i)]:
+    if nums[counting(i, primes)]:
         ans += 1
 
 print(ans)
