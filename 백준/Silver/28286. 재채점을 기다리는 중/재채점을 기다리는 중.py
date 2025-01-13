4ad67928cdb02ca20261b32e7aee117d60300311
mediwind <48349059+mediwind@users.noreply.github.com>
2025-01-14 00:51:01 +0900

    [Silver II] Title: 재채점을 기다리는 중, Time: 272 ms, Memory: 112016 KB -BaekjoonHub

---
+++ 백준/Silver/28286. 재채점을 기다리는 중/재채점을 기다리는 중.py
@@ -0,0 +1,32 @@
+def DFS(L, arr):
+    global ans
+    
+    if L > k:
+        return
+    
+    cnt = 0
+    
+    # 그냥 놓아두기 & 정답 개수 세기
+    for i in range(n):
+        if arr[i] == answer[i]:
+            cnt += 1
+    
+    ans = max(ans, cnt)
+    
+    for i in range(n):
+        # 당기기
+        tmp = arr[:i] + arr[i + 1:] + [0]
+        DFS(L + 1, tmp)
+        # 밀기
+        tmp = arr[:i] + [0] + arr[i:-1]
+        DFS(L + 1, tmp)
+
+
+n, k = map(int, input().split())
+answer = list(map(int, input().split()))
+submit = list(map(int, input().split()))
+
+ans = 0
+cnt = 0
+DFS(0, submit)
+print(ans)
