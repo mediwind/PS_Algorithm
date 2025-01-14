6cd7085de7f7a7ec75da729f97dd5918c6a20db3
mediwind <48349059+mediwind@users.noreply.github.com>
2025-01-14 09:46:08 +0900

    Time: 2 ms (15.8%), Space: 18.4 MB (6.41%) - LeetHub

---
+++ LeetCode/Easy/1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        n = len(coordinates)
+        res = set()
+        for i in range(n - 1):
+            x1, y1 = coordinates[i]
+            x2, y2 = coordinates[i + 1]
+            if x2 == x1:
+                res.add(float("inf"))
+                continue
+            tmp = (y2 - y1) / (x2 - x1)
+            res.add(tmp)
+        
+        if len(res) >= 2:
+            return False
+        return True
