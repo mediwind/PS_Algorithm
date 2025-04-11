ad852975453213e51cebc22096b774d8b5a4c3b6
mediwind <48349059+mediwind@users.noreply.github.com>
2025-04-11 20:11:49 +0900

    [Bronze IV] Title: RACI, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32905. RACI/RACI.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+flag = True
+
+for _ in range(n):
+    arr = list(input().split())
+
+    if arr.count("A") != 1:
+        flag = False
+
+if flag:
+    print("Yes")
+else:
+    print("No")
