9c0e44bc705f33331938ab7fd179eae52b249118
mediwind <48349059+mediwind@users.noreply.github.com>
2025-07-28 10:14:51 +0900

    Time: 0 ms (100%), Space: 17.8 MB (54.37%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.py
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -1,11 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
 
-        # [rob1, rob2, n, n + 1, ...]
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
 
-        return rob2
+        for i in range(2, n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+        return dp[-1]
