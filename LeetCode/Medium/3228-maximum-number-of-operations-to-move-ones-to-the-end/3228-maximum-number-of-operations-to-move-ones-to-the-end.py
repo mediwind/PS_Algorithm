912bd3f9065efd2b7ac3f83f53645ceb99b0f12f
mediwind <48349059+mediwind@users.noreply.github.com>
2025-11-13 23:56:46 +0900

    Time: 59 ms (67.07%), Space: 18.1 MB (81.44%) - LeetHub

---
+++ LeetCode/Medium/3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxOperations(self, s: str) -> int:
+        ones = 0
+        ans = 0
+        for i, c in enumerate(s):
+            if c == '1':
+                ones += 1
+            elif i > 0 and s[i - 1] == '1':
+                ans += ones
+
+        return ans
