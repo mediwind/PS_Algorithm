b065d1df3a43cab81749b7fc30adf13a8be305c0
mediwind <48349059+mediwind@users.noreply.github.com>
2025-12-04 23:16:04 +0900

    Time: 53 ms (86.25%), Space: 18.4 MB (56.88%) - LeetHub

---
+++ LeetCode/Medium/2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        n = len(directions)
+        
+        i = 0
+        while i < n and directions[i] == 'L':
+            i += 1
+
+        j = n - 1
+        while j >= 0 and directions[j] == 'R':
+            j -= 1
+
+        if i > j:
+            return 0
+
+        return sum(1 for c in directions[i:j + 1] if c != 'S')
