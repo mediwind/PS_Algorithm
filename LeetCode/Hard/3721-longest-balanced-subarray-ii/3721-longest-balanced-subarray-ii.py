ec1039046d6b5837bdd4b3d4281312fbd1e078a6
mediwind <48349059+mediwind@users.noreply.github.com>
2026-02-11 22:54:52 +0900

    Time: 10467 ms (43.66%), Space: 50 MB (36.62%) - LeetHub

---
+++ LeetCode/Hard/3721-longest-balanced-subarray-ii/3721-longest-balanced-subarray-ii.py
@@ -0,0 +1,82 @@
+class SegmentTree:
+
+    def __init__(self, n: int):
+        self.n = n
+        self.size = 4 * n
+        self.sum = [0] * self.size
+        self.min = [0] * self.size
+        self.max = [0] * self.size
+
+    def _pull(self, node: int):
+
+        l, r = node * 2, node * 2 + 1
+
+        self.sum[node] = self.sum[l] + self.sum[r]
+        self.min[node] = min(self.min[l], self.sum[l] + self.min[r])
+        self.max[node] = max(self.max[l], self.sum[l] + self.max[r])
+
+    def update(self, idx: int, val: int):
+
+        def _update(node: int = 1, l: int = 0, r: int = self.n - 1):
+            if l == r:
+                self.sum[node] = val
+                self.min[node] = val
+                self.max[node] = val
+                return
+
+            m = l + (r - l) // 2
+            if idx <= m:
+                _update(node * 2, l, m)
+            else:
+                _update(node * 2 + 1, m + 1, r)
+
+            self._pull(node)
+
+        return _update()
+
+    def find_rightmost_prefix(self, target: int = 0) -> int:
+
+        def _exist(node: int, sum_before: int):
+            return self.min[node] <= target - sum_before <= self.max[node]
+
+        def _find(node: int = 1, l: int = 0, r: int = self.n - 1, sum_before: int = 0):
+            if not _exist(node, sum_before):
+                return -1
+            if l == r:
+                return l
+
+            m = l + (r - l) // 2
+            lchild, rchild = node * 2, node * 2 + 1
+
+            
+            sum_before_right = self.sum[lchild] + sum_before
+            if _exist(rchild, sum_before_right):
+                return _find(rchild, m + 1, r, sum_before_right)
+
+            return _find(lchild, l, m, sum_before)
+
+        return _find()
+
+
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        stree = SegmentTree(n)  
+        first = dict()  
+
+        result = 0
+        for l in reversed(range(n)):
+            num = nums[l]
+    
+            if num in first:
+                stree.update(first[num], 0)
+
+            first[num] = l
+            stree.update(l, 1 if num % 2 == 0 else -1)
+
+            r = stree.find_rightmost_prefix(target=0)
+            if r >= l:
+                result = max(result, r - l + 1)
+
+        return result
