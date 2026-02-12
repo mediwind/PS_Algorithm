e0e741a216b966662452b9d84e15eb699067f5eb
mediwind <48349059+mediwind@users.noreply.github.com>
2026-02-12 23:56:08 +0900

    Time: 1060 ms (94.16%), Space: 19.4 MB (20.27%) - LeetHub

---
+++ LeetCode/Medium/3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        cnt, n = 1, len(s)
+
+        for l in range(n):
+            freq = [0] * 26
+            uniq, maxF, cntMax = 0, 0, 0
+
+            for r in range(l, n):
+                idx = ord(s[r]) - 97
+                freq[idx] += 1
+                f = freq[idx]
+
+                uniq += f == 1
+
+                if f > maxF:
+                    maxF = f
+                    cntMax = 1
+                elif f == maxF:
+                    cntMax += 1
+
+                if uniq == cntMax:
+                    cnt = max(cnt, r - l + 1)
+
+        return cnt
