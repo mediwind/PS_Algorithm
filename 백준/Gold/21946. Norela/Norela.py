5b7ed24a4cf82a32225dacb93de8737c551150eb
mediwind <48349059+mediwind@users.noreply.github.com>
2026-03-19 23:52:59 +0900

    [Gold IV] Title: Norela, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/21946. Norela/Norela.py
+++ 백준/Gold/21946. Norela/Norela.py
@@ -4,41 +4,73 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 spells = []
-
 for _ in range(m):
     data = list(map(int, input().split()))
-    q = data[0]
-    cards = data[1:]
-
     mask = 0
-    for card in cards:
+    # 비트마스킹으로 마법 상태 저장
+    for card in data[1:]:
         mask |= (1 << (card - 1))
-
     spells.append(mask)
 
 target = (1 << n) - 1
 
-best_count = float('inf')
-best_sequence = []
+# 전체 마법을 절반으로 나눕니다.
+mid = m // 2
 
-for subset in range(1 << m):
+# 왼쪽 절반 탐색 (0 ~ mid-1)
+left_memo = {}
+for subset in range(1 << mid):
     state = 0
-    used_spells = []
-
-    for i in range(m):
+    used = []
+    
+    for i in range(mid):
         if subset & (1 << i):
             state ^= spells[i]
-            used_spells.append(i + 1)
+            used.append(i + 1)
+            
+    count = len(used)
+    used_tuple = tuple(used)
+    
+    # 딕셔너리에 최소 개수, 사전순 최적의 조합만 저장합니다.
+    if state not in left_memo:
+        left_memo[state] = (count, used_tuple)
+    else:
+        prev_count, prev_tuple = left_memo[state]
+        if count < prev_count:
+            left_memo[state] = (count, used_tuple)
+        elif count == prev_count and used_tuple < prev_tuple:
+            left_memo[state] = (count, used_tuple)
 
-    if state == target:
-        count = len(used_spells)
+best_count = float('inf')
+best_sequence = ()
 
-        if count < best_count:
-            best_count = count
-            best_sequence = used_spells
-        elif count == best_count:
-            if used_spells < best_sequence:
-                best_sequence = used_spells
+# 오른쪽 절반 탐색 (mid ~ m-1)
+right_size = m - mid
+for subset in range(1 << right_size):
+    state = 0
+    used = []
+    
+    for i in range(right_size):
+        if subset & (1 << i):
+            state ^= spells[mid + i]
+            used.append(mid + i + 1)
+            
+    # 전체를 target으로 만들기 위해 왼쪽에서 필요한 상태
+    req_state = target ^ state
+    
+    if req_state in left_memo:
+        left_count, left_tuple = left_memo[req_state]
+        total_count = left_count + len(used)
+        
+        # 왼쪽 절반의 인덱스가 항상 작으므로 단순 연결만 해도 오름차순이 유지됩니다.
+        total_sequence = left_tuple + tuple(used)
+        
+        # 최적의 결과 갱신
+        if total_count < best_count:
+            best_count = total_count
+            best_sequence = total_sequence
+        elif total_count == best_count and total_sequence < best_sequence:
+            best_sequence = total_sequence
 
 print(best_count)
 print(*best_sequence)
