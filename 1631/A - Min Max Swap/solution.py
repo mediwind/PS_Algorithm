89b6b7f82f77c2f1d6ec63be7212491fb7c359a9
mediwind <48349059+mediwind@users.noreply.github.com>
2026-06-11 00:00:40 +0900

    Add Min Max Swap [A] from Codeforces

---
+++ 1631/A - Min Max Swap/solution.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+ 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    
+    for i in range(n):
+        if a[i] < b[i]:
+            a[i], b[i] = b[i], a[i]
+            
+    print(max(a) * max(b))
