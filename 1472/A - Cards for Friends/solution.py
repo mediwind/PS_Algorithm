1768fc905d2fac3d87478c17f8a12554e02d91b7
mediwind <48349059+mediwind@users.noreply.github.com>
2026-06-20 23:02:12 +0900

    Add Cards for Friends [A] from Codeforces

---
+++ 1472/A - Cards for Friends/solution.py
@@ -0,0 +1,21 @@
+import sys
+# input = sys.stdin.readline
+ 
+t = int(input())
+for _ in range(t):
+    w, h, n = map(int, input().split())
+    
+    pieces = 1
+    
+    while w % 2 == 0:
+        w //= 2
+        pieces *= 2
+    
+    while h % 2 == 0:
+        h //= 2
+        pieces *= 2
+    
+    if pieces >= n:
+        print("YES")
+    else:
+        print("NO")
