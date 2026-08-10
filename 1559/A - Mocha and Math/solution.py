14cfb05acb1186faa8a3320d70adda8cf0c240c3
mediwind <48349059+mediwind@users.noreply.github.com>
2026-08-10 23:58:03 +0900

    Add Mocha and Math [A] from Codeforces

---
+++ 1559/A - Mocha and Math/solution.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+ 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    ans = a[0]
+    for i in range(1, n):
+        ans &= a[i]
+        
+    print(ans)
