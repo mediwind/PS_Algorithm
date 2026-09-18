ff3bf57dcc027e77b6dca7acb71730921696353b
mediwind <48349059+mediwind@users.noreply.github.com>
2026-09-18 23:29:55 +0900

    Add Even Odds [A] from Codeforces

---
+++ 318/A - Even Odds/solution.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+ 
+n, k = map(int, input().split())
+ 
+odd_count = (n + 1) // 2
+ 
+if k <= odd_count:
+    print(2 * k - 1)
+else:
+    print(2 * (k - odd_count))
